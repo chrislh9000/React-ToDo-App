b889a0990048ea44213d38a704c1ca6db55e422d
Christopher Hemm <chrishemm@gmail.com>
2018-07-26 15:31:04 -0700

    added to-do items

--- reactApp/components/InputLine.js
+++ reactApp/components/InputLine.js
@@ -2,11 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class InputLine extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      typedText: '',
+    }
+  }
+  handleTyping(event) {
+    this.setState({
+      typedText: event.target.value,
+    })
+  }
+
+  handleSubmit() {
+    this.props.submit(this.state.typedText);
+    this.setState({
+      typedText: '',
+    })
+  }
+
+
   render() {
     return(
       <div>
-      <input className="input-task" type="text" placeholder="Write ToDo..." />
-      <input onClick={() => this.props.submit('Test Task')} className="btn btn-primary" type="submit" value="submit"/>
+      <input onChange={(event)=> this.handleTyping(event)} value={this.state.typedText} className="input-task" type="text" placeholder="Write ToDo..." />
+      <input onClick={() => this.handleSubmit()} className="btn btn-primary" type="submit" value="submit"/>
       </div>
     )
   }
