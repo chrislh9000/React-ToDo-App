cbeef1bb215d5f0e4ee17621339e0b9f0be176c1
Christopher Hemm <chrishemm@gmail.com>
2018-07-26 14:10:40 -0700

    added buttons

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>React lives!</p>,
-document.getElementById('root'));
+const dummyData = ['Clean Dishes', 'Watch TV', 'Sleep', 'Code'];
+dummyData.map((task) => {
+  console.log(task);
+})
+//To do class
+class ToDo extends React.Component {
+  // maybe a constructor is needed but I don't foresee children here
+  render() {
+    return(
+      <div>
+      <li><button>X</button>{this.props.task}</li>
+      </div>
+    )
+  }
+}
+
+//To do list components
+
+class TodoList extends React.Component {
+  constructor(props){
+    super(props)
+  }
+  render() {
+    return(
+      <ul>
+       {dummyData.map((todo) => (
+        <ToDo task={todo} />
+      ))}
+      </ul>
+    )
+  }
+}
+
+
+
+ReactDOM.render(<TodoList dummyData={dummyData} />,
+  document.getElementById('root'));
