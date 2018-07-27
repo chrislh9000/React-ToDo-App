a1b9358043c2083977995f0ff942bf2e767df542
Christopher Hemm <chrishemm@gmail.com>
2018-07-26 22:49:03 -0700

    mlab works

--- reactApp/backend/server.js
+++ reactApp/backend/server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 const dbRoutes = require('./routes/databaseAccess.js');
+const mongoose = require('mongoose');
+const connect = process.env.MONGODB_URI;
+
+
+
+mongoose.connection.on('error', function() {
+  console.log('error connecting to database')
+})
+mongoose.connection.on('connected', function() {
+  console.log('succesfully connected to database')
+})
+
+mongoose.connect(connect);
 
 // This line makes the build folder publicly available.
 app.use(express.static('build'));
